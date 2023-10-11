3fdd21933f140bd512eba4a9fd1e8a019d074b2d
220031 <220031pu@yse-c.net>
2023-10-11 10:52:25 +0900

    node

--- express_sample/server.js
+++ express_sample/server.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT
 // サーバ作成
 const app = express()
 
+// ミドルウェアの設定
+// publicフォルダを静的コンテンツのフォルダに設定
+app.use(express.static(__dirname + '/public'))
+
 // GETリクエストの処理
 app.get('/', (req, res) => {
     // リクエストの処理
@@ -20,6 +24,10 @@ app.get('/', (req, res) => {
 
     // レスポンスの処理
     res.send('Hello!!!!!!')
+})
+
+app.get('/profile', (req, res) => {
+    res.send('プロフィール')
 })
 
 //　サーバ停止: 起動中のターミナルで Ctrl + C
